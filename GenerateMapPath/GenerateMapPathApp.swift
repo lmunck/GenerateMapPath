3c6c13e5984f31c58f2d2b858eab2396ba45c817
lmunck <anders.munck@gmail.com>
2021-03-08 21:26:10 +0100

    Initial Commit

---
+++ GenerateMapPath/GenerateMapPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerateMapPathApp.swift
+//  GenerateMapPath
+//
+//  Created by Anders Munck on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct GenerateMapPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
